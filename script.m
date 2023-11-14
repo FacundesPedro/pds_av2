f468d6c65f931a036de6ebea41054448dc7a5c6c
FacundesPedro <pedro.f.facundes1@gmail.com>
2023-11-14 13:48:17 -0300

    [refactor]: chaing for apresentation, uncommet out.wav, adding filtered signals

--- script.m
+++ script.m
@@ -11,17 +11,18 @@ time = 5; % record duration in seconds
 
 % Criando um sinal senoidal de teste!!!!
 t = 0:1/6000:time; % Vetor de tempo
-f = 150; % Frequência do sinal senoidal
-x = sin(2*pi*f*t);
-noise = randn(1,length(x));
+%f = 150; % Frequência do sinal senoidal
+%x = sin(2*pi*f*t);
+%noise = randn(1,length(x));
 %y + noise
-signal = x + noise;
+%signal = x + noise;
+
 % uncomment this if want to create a new file;
 % add print to show when the process start!
-
+filename = "out";
 %setRec("out.wav", 5, fs);
 %% Substituir pelo sinal de voz gravado
-%[x, ] = getRec(); %fs was defined in the start of the script
+[x, ] = getRec(strcat(filename, '.wav')); %fs was defined in the start of the script
 
 % FIR
 ripple_max = 1; % Ripple máximo na banda de passagem (em dB)
@@ -30,8 +31,6 @@ transition_width = 1; % Largura da banda de transição (em oitavas)
 
 % IRR
 
-%% Substituir pelo sinal de voz gravado
-%[x, ] = getRec(); %fs was defined in the start of the script
 
 % frequências de corte
 passband_low_freq = fc_low; % banda de passagem 
@@ -60,9 +59,9 @@ passband_high_normalized = passband_high_freq / (fs/2);
 stopband_high_normalized = stopband_high_freq / (fs/2);
 
 % Calculando a ordem do filtro usando a regra de Kaiser
-order_fir_low = kaiserord([passband_low_normalized, stopband_low_normalized], [1, 0], [ripple_max, attenuation]);
-order_fir_medium = kaiserord([passband_medium_normalized, stopband_medium_normalized], [1, 0], [ripple_max, attenuation]);
-order_fir_high = kaiserord([passband_high_normalized, stopband_high_normalized], [1, 0] , [ripple_max, attenuation]);
+%order_fir_low = kaiserord([passband_low_normalized, stopband_low_normalized], [1, 0], [ripple_max, attenuation]);
+%order_fir_medium = kaiserord([passband_medium_normalized, stopband_medium_normalized], [1, 0], [ripple_max, attenuation]);
+%order_fir_high = kaiserord([passband_high_normalized, stopband_high_normalized], [1, 0] , [ripple_max, attenuation]);
 order_fir_low = 550;
 order_fir_medium = 550;
 order_fir_high = 550;
@@ -77,34 +76,72 @@ fir_high_filter = fir1(order_fir_high, [passband_high_normalized, stopband_high_
 [b_low, a_low] = butter(6, stopband_low_normalized, 'low');
 
 % Aplicação do filtro IIR passa-baixa no sinal de áudio gravado
-voiceFilteredLowIIR = filter(b_low, a_low, signal);
+voiceFilteredLowIIR = filter(b_low, a_low, x);
 
 % Projeto do filtro IIR passa-faixa
 [b_mid, a_mid] = butter(6, [stopband_medium_normalized, stopband_high_normalized], 'bandpass');
 
 % Aplicação do filtro IIR passa-faixa no sinal de áudio gravado
-voiceFilteredMidIIR = filter(b_mid, a_mid, signal);
+voiceFilteredMidIIR = filter(b_mid, a_mid, x);
 
 % Projeto do filtro IIR passa-alta
 [b_high, a_high] = butter(6, stopband_high_normalized, 'high');
 
 % Aplicação do filtro IIR passa-alta no sinal de áudio gravado
-voiceFilteredHighIIR = filter(b_high, a_high, signal);
+voiceFilteredHighIIR = filter(b_high, a_high, x);
 
 % Aplicando filtros
-filtered_low_fir = filter(fir_low_filter, 1, signal);
-filtered_medium_fir = filter(fir_medium_filter, 1, signal);
-filtered_high_fir = filter(fir_high_filter, 1, signal);
+filtered_low_fir = filter(fir_low_filter, 1, x);
+filtered_medium_fir = filter(fir_medium_filter, 1, x);
+filtered_high_fir = filter(fir_high_filter, 1, x);
+
+%Resposta ao Impulso para filtros FIR
+impulse_response_low_fir = impz(fir_low_filter);
+impulse_response_medium_fir = impz(fir_medium_filter);
+impulse_response_high_fir = impz(fir_high_filter);
+
+%Resposta ao Impulso para filtros IIR
+impulse_response_low_iir = impz(b_low, a_low);
+impulse_response_medium_iir = impz(b_mid, a_mid);
+impulse_response_high_iir = impz(b_high, a_high);
+
+%Plotagem resposta ao impulso para filtros FIR
+figure;
+subplot(3,1,1);
+stem(impulse_response_low_fir);
+title('Resposta ao Impulso - FIR|LOW');
+
+subplot(3,1,2);
+stem(impulse_response_medium_fir);
+title('Resposta ao Impulso - FIR|MEDIUM');
+
+subplot(3,1,3);
+stem(impulse_response_high_fir);
+title('Resposta ao Impulso - FIR|HIGH');
+
+% Plotagem resposta ao impulso para filtros IIR
+figure;
+subplot(3,1,1);
+stem(impulse_response_low_iir);
+title('Resposta ao Impulso - IIR|LOW');
+
+subplot(3,1,2);
+stem(impulse_response_medium_iir);
+title('Resposta ao Impulso - IIR|MEDIUM');
+
+subplot(3,1,3);
+stem(impulse_response_high_iir);
+title('Resposta ao Impulso - IIR|HIGH');
 
 % plotagem sinal de entrada e ruido
 figure;
 subplot(2,1,1);
-plot(t, signal);
-title('Sinal com ruído');
+plot(x);
+title('Original');
 
 subplot(2,1,2);
-plot(t, x);
-title('Sinal Original');
+stem(abs(fft(x)));
+title('Sinal Original (Magnitude)');
 
 % Plotagem FIR FIltros
 figure;
@@ -118,28 +155,32 @@ title('Filtro Passa-faixa ( 250-2000 hz )');
 figure;
 freqz(fir_high_filter, 1, 1024, fs);
 title('Filtro Passa-Alta ( 2000/20000 hz )');
+
 % Plotagem FIR Sinais filtrados
 figure;
-subplot(2,1,1);
-plot(t, filtered_low_fir);
+subplot(3,1,1);
+stem(filtered_low_fir);
 title('Sinal Filtrado com FIR/LOW');
 
-subplot(2,1,2);
-plot(t, filtered_medium_fir);
+subplot(3,1,2);
+stem(filtered_medium_fir);
 title('Sinal Filtrado com FIR/MEDIUM');
 
-figure;
-subplot(2,1,1);
-plot(t, filtered_high_fir);
+subplot(3,1,3);
+stem(filtered_high_fir);
 title('Sinal Filtrado com FIR/HIGH');
 
 % Plotagem dos sinais filtrados IIR
 figure;
-
 subplot(3,1,1);
-plot(t, voiceFilteredLowIIR);
+stem(voiceFilteredLowIIR);
 title('Sinal Filtrado com IIR/Baixo');
 
 subplot(3,1,2);
-plot(t, voiceFilteredMidIIR);
+stem(voiceFilteredMidIIR);
 title('Sinal Filtrado com IIR/Médio-Alto');
+
+% Save the filtered signal to a .wav file
+audiowrite(strcat(filename,"_fir_low.wav"), filtered_low_fir, fs);
+audiowrite(strcat(filename,"_fir_medium.wav"), filtered_medium_fir, fs);
+audiowrite(strcat(filename,"_fir_high.wav"), filtered_high_fir, fs);
